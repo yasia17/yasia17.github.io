b1aeca822cc27be7cb8f84a13b89b521d5c9e45d
yasia17 <63717982+yasia17@users.noreply.github.com>
2022-01-24 20:12:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// function hidden(a){
+//   if (a == 1) {
+//     document.getElementsByName("startBtn")[0].style.visibility = "hidden";
+//   }
+//   else if (a == 2) {
+//   document.getElementsByName("startBtn")[0].style.visibility = "visible";
+//   }
+// }
+
+
+function myFunction() {
+
+  var person = prompt("Please enter your name");
+  document.getElementById("p1").innerHTML = "hello " + person + ", welcome to our website! we are all about traveling around the world so even when covid 19 is here you will still be able to find cool places to visit press start to choose where to go";
+  document.getElementsByName("startBtn")[0].style.visibility = "visible";
+  // <button onclick="myFunction1()">Click me</button>
+}
+
+function myFunction1() {
+  location.replace("continents.html");
+}
+
+function europe1() {
+  location.replace("europe.html");
+}
+
+function restaurant_eu() {
+  location.replace("restaurants_eu.html");
+}
+
+function na() {
+  location.replace("NA.html");
+}
+
+function hotels_eu() {
+  location.replace("hotels_eu.html");
+}
+
+function museums_eu() {
+  location.replace("museums_eu.html");
+}
+
+function restaurant_NA() {
+  location.replace("restaurants_NA.html");
+}
+
+
+
+
+
+document.getElementsByName("startBtn")[0].style.visibility = "hidden";
+myFunction();
+
+// put myFunction in a start function which will have a start button
+
+// <button name="btnValidateWork">my button?</button>
+
