af5bcc5621c94f7365df9e4248a46d2fe0cad1ff
yasia17 <63717982+yasia17@users.noreply.github.com>
2022-01-26 16:03:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,21 @@
 // }
 
 
+// function myFunction() {
+
+//   var person = prompt("Please enter your name");
+//   document.getElementById("p1").innerHTML = "hello " + person + "! this website is all about traveling around the world so even while covid 19 is here you will still be able to find cool places to visit for the future. press start to choose where to go";
+//   document.getElementsByName("startBtn")[0].style.visibility = "visible";
+//   // <button onclick="myFunction1()">Click me</button>
+// }
+
 function myFunction() {
 
-  var person = prompt("Please enter your name");
-  document.getElementById("p1").innerHTML = "hello " + person + ", welcome to our website! we are all about traveling around the world so even when covid 19 is here you will still be able to find cool places to visit press start to choose where to go";
+  var person =  document.getElementById("p1").innerHTML = "hello! this website is all about traveling around the world so even while covid 19 is here you will still be able to find cool places to visit for the future. press start to choose where to go";
   document.getElementsByName("startBtn")[0].style.visibility = "visible";
   // <button onclick="myFunction1()">Click me</button>
 }
+
 
 function myFunction1() {
   location.replace("continents.html");
@@ -54,4 +62,3 @@ myFunction();
 // put myFunction in a start function which will have a start button
 
 // <button name="btnValidateWork">my button?</button>
-
